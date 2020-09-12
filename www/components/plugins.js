2b0a58c39b33b6d8e6924beeb5bba67788bf8803
ClaudHelloworld <fantasmagomes999@gmail.com>
2020-09-13 06:24:44 +0900

    att

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,8 +1,21 @@
 // This is a JavaScript file
 
 $(document).on("click", "#alerta", function(){
-function retorno(){
+navigator.notification.alert("Minha Mensagem",null,"aviso","aceito");
+});
 
-}
-navigator.notification.alert("Minha Mensagem");
+$(document).on("click", "#alerta", function(){
+  function confirm(buttonIndex){
+    if(buttonIndex == 1){
+       navigator.notification.alert("Escolheu a opção A");
+    }
+   else{
+       navigator.notification.alert("Escolheu a opção B");
+   }
+  }
+navigator.notification.confirm("escolha A ou B",confirm,"Escolha"['A','B']);
+});
+
+$(document).on("click", "#beep", function(){
+navigator.notification.beep(3);
 });
