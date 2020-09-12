c9b2970375551a6b0b1cbb879cc6631bd52624d2
ClaudHelloworld <fantasmagomes999@gmail.com>
2020-09-13 06:59:26 +0900

    att

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -19,3 +19,36 @@ navigator.notification.confirm("escolha A ou B",confirm,"Escolha"['A','B']);
 $(document).on("click", "#beep", function(){
 navigator.notification.beep(3);
 });
+
+$(document).on("click", "#vibrar", function(){
+navigator.vibrate(3000);
+});
+
+
+ 
+function mostramapa(lat, long){
+  
+   L.mapquest.key = 'fClYReR5tspIzkh0ORT0u5Ae7OzPg5MG';
+
+        var map = L.mapquest.map('map', {
+          center: [lat, long],
+          layers: L.mapquest.tileLayer('map'),
+          zoom: 12
+});
+
+ map.addControl(L.mapquest.control());}
+
+$(document).on("click", "#local", function(){
+ var onSuccess = function(position) {
+   mostramapa(position.coords.latitude,position.coords.longitude) };
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+});
+
